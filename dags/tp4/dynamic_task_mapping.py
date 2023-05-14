6ad262952b0ef449353bde5613be2c7538e3c22f
mariaafara <maria-afara5@hotmail.com>
2023-05-14 23:50:33 +0200

    draft dynamic_task_mapping

--- dags/tp4/dynamic_task_mapping.py
+++ dags/tp4/dynamic_task_mapping.py
@@ -1,58 +1,23 @@
-from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.python_operator import PythonOperator
+from airflow.decorators import dag, task, task_group
+
 from datetime import datetime
 
 
-# Define the function that will be used as a callback for dynamic task mapping
-def my_dynamic_task_mapping(ds, **kwargs):
-    # Extract the task instance from the kwargs
-    task_instance = kwargs['task_instance']
-
-    # Get the current task's name
-    current_task = task_instance.task_id
-
-    # Define the mapping of tasks based on the current task's name
-    task_mapping = {
-        'task1': 'task_a',
-        'task2': 'task_b',
-        'task3': 'task_c'
-    }
-
-    # Check if the current task has a mapping in the task_mapping dictionary
-    if current_task in task_mapping:
-        # Get the mapped task
-        mapped_task = task_mapping[current_task]
-
-        # Set the mapped task as the current task's downstream task
-        task_instance.xcom_push(key='mapped_task', value=mapped_task)
-
-
 # Define the DAG
-dag = DAG(
-    'dynamic_task_mapping_example',
+@dag(
+    'dynamic_task_mapping_example1',
     start_date=datetime(2023, 5, 10),
     schedule_interval=None
 )
+def my_dag():
+    @task
+    def add(x: int, y: int):
+        return x + y
 
-# Define the tasks
-start_task = DummyOperator(task_id='start_task', dag=dag)
+    # This expand function creates three mapped add tasks, one for each entry in the x input list.
+    # The partial function specifies a value for y that remains constant in each task.
+    added_values = add.partial(y=10).expand(x=[1, 2, 3])
 
-# Dynamic task mapping task
-dynamic_mapping_task = PythonOperator(
-    task_id='dynamic_mapping_task',
-    provide_context=True,
-    python_callable=my_dynamic_task_mapping,
-    dag=dag
-)
 
-# Dummy tasks for demonstration
-task_a = DummyOperator(task_id='task_a', dag=dag)
-task_b = DummyOperator(task_id='task_b', dag=dag)
-task_c = DummyOperator(task_id='task_c', dag=dag)
-
-end_task = DummyOperator(task_id='end_task', dag=dag)
-
-# Define the task dependencies using dynamic mapping
-start_task >> dynamic_mapping_task >> [task_a, task_b, task_c] >> end_task
-
+# Call the my_dag function to create an instance of the DAG
+my_dag()
